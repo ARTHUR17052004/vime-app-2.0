0560d02ca418a00c4957a0eb1b1c50637d5c45fc
Pedro Afonso <ycomicas@gmail.com>
2026-06-14 21:49:27 -0300

    fim do dia 7 segurança do locador realizada

---
+++ backend/prisma/migrations/20260614214712_sync_kitnet_frontend/migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "Kitnet"
+RENAME COLUMN "valorAluguel" TO "aluguel";
+
+ALTER TABLE "Kitnet"
+ADD COLUMN "nome" TEXT;
--- backend/src/routes/locadorRoutes.js
+++ backend/src/routes/locadorRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const perfilMiddleware = require('../middlewares/perfilMiddleware');
+
 const router = express.Router();
 
 const locadorController = require('../controllers/locadorController');
 
-router.get('/', locadorController.listar);
+router.get('/', authMiddleware, perfilMiddleware('ADMINISTRADOR'), locadorController.listar);
 
-router.post('/', locadorController.criar);
+router.post('/', authMiddleware, perfilMiddleware('ADMINISTRADOR'), locadorController.criar);
 
-router.put('/:id', locadorController.atualizar);
+router.put('/:id', authMiddleware, perfilMiddleware('ADMINISTRADOR'), locadorController.atualizar);
 
-router.delete('/:id', locadorController.remover);
+router.delete('/:id', authMiddleware, perfilMiddleware('ADMINISTRADOR'), locadorController.remover);
 
 module.exports = router;
