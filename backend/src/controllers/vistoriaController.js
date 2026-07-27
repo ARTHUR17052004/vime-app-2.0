260b1f23e171a6ab8b7f87a63b6a2e64fe2f1af4
Pedro Afonso <ycomicas@gmail.com>
2026-07-27 18:45:37 -0300

    teste de push

---
+++ backend/src/controllers/vistoriaController.js
@@ -0,0 +1,54 @@
+const vistoriaService = require('../services/vistoriaService');
+
+const listar = async (req, res) => {
+  const dados = await vistoriaService.listar();
+  res.json(dados);
+};
+
+const buscarPorId = async (req, res) => {
+
+  const vistoria = await vistoriaService.buscarPorId(req.params.id);
+
+  if (!vistoria) {
+    return res.status(404).json({
+      mensagem: 'Vistoria não encontrada.'
+    });
+  }
+
+  res.json(vistoria);
+
+};
+
+const criar = async (req, res) => {
+  const vistoria = await vistoriaService.criar(req.body);
+  res.status(201).json(vistoria);
+};
+
+const atualizar = async (req, res) => {
+
+  const vistoria = await vistoriaService.atualizar(
+    req.params.id,
+    req.body
+  );
+
+  res.json(vistoria);
+
+};
+
+const remover = async (req, res) => {
+
+  await vistoriaService.remover(req.params.id);
+
+  res.json({
+    mensagem: 'Vistoria removida com sucesso.'
+  });
+
+};
+
+module.exports = {
+  listar,
+  buscarPorId,
+  criar,
+  atualizar,
+  remover
+};
